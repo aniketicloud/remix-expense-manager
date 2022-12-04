36f58d93eb7df4f97486dcf72b8eab7a0042d571
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-04 12:34:33 +0530

    Nested Paths - An Alternative Approach

--- app/routes/expenses/index.jsx
+++ app/routes/expenses/index.jsx
@@ -1,8 +1,10 @@
+// /expenses
+
 // const Expensespage = () => <h1>Expenses Page</h1>;
 export default function ExpensesPage() {
   return (
     <div>
-      <h1>Add Expenses Page</h1>
+      <h1>Expenses Page</h1>
     </div>
   );
 }
