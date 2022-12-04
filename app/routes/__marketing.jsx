7f1552296a20a9c0f0221c03a22de546f08622ab
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-04 23:21:33 +0530

    Doing More with Pathless Layout Routes part-2

---
+++ app/routes/__marketing.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "@remix-run/react";
+import marketingStyles from "~/styles/marketing.css";
+
+export default function MarketingLayout() {
+  return <Outlet />;
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: marketingStyles }];
+}
