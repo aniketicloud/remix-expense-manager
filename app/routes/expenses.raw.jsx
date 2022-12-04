9ae2abdf0b14b65f469d95630b30e7f23d47b37e
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-04 23:35:49 +0530

    Introducing Resource Routes

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,9 +1,20 @@
 // /expenses/raw
 
-export default function ExpensesRawPage() {
-  return (
-    <div>
-      <h1>Expenses raw Page</h1>
-    </div>
-  );
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export function loader() {
+  return DUMMY_EXPENSES;
 }
