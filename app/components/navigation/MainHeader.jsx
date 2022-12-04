3fd13e9c9f78342ca3a5ece7c24628dbe6ac4a2c
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-04 16:41:12 +0530

    Adding NavLinks

--- app/components/navigation/MainHeader.jsx
+++ app/components/navigation/MainHeader.jsx
@@ -1,4 +1,5 @@
-import Logo from '../util/Logo';
+import { Link, NavLink } from "@remix-run/react";
+import Logo from "../util/Logo";
 
 function MainHeader() {
   return (
@@ -7,22 +8,22 @@ function MainHeader() {
       <nav id="main-nav">
         <ul>
           <li>
-            <a href="/">Home</a>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <a href="/pricing">Pricing</a>
+            <NavLink to="/pricing">Pricing</NavLink>
           </li>
           <li>
-            <a href="/expenses">Expenses</a>
+            <NavLink to="/expenses">Expenses</NavLink>
           </li>
         </ul>
       </nav>
       <nav id="cta-nav">
         <ul>
           <li>
-            <a href="/auth" className="cta">
+            <Link to="/auth" className="cta">
               Login
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
