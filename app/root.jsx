b61e867d2e68baea0c99003941988a0acaacac2a
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-04 13:20:51 +0530

    Using Custom Fonts

--- app/root.jsx
+++ app/root.jsx
@@ -20,6 +20,16 @@ export default function App() {
     <html lang="en">
       <head>
         <Meta />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <Links />
       </head>
       <body>
