870cbc5382698712bfaddee5dde1b430aeca3570
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-11 19:14:15 +0530

    Extracting Search Parameters

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -5,6 +5,20 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get("mode") || "login";
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  if (authMode === "login") {
+    // login logic
+  } else {
+    // signup logic (create user)
+  }
+}
+
 export function links() {
   return [{ rel: "stylesheet", href: authStyles }];
 }
