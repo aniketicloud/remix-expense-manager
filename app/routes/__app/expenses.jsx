5970aa0bed143f08a09a0fcfb1ff5840e484cf96
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-12-11 23:17:36 +0530

    Handling Errors Without ErrorBoundary or CatchBoundary

--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -8,6 +8,8 @@ import { getExpenses } from "~/data/expenses.server";
 export default function ExpensesLayout() {
   const expenses = useLoaderData();
 
+  const hasExpenses = expenses && expenses.length > 0;
+
   return (
     <>
       <Outlet />
@@ -22,12 +24,45 @@ export default function ExpensesLayout() {
             <span>Load Raw Data</span>
           </a>
         </section>
-        <ExpensesList expenses={expenses} />
+        {hasExpenses ? (
+          <ExpensesList expenses={expenses} />
+        ) : (
+          <section id="no-expenses">
+            <h1>No expenses found</h1>
+            <p>
+              Start <Link to="add">adding some today</Link>
+            </p>
+          </section>
+        )}
       </main>
     </>
   );
 }
 
-export function loader({ request }) {
-  return getExpenses();
+export async function loader() {
+  const expenses = await getExpenses();
+
+  // json is remix utility funtion
+  // here we can't throw an error response, as it will ...
+  // ...render closest CatchBoundary
+  // So, we will return expenses as it is and manage it on frontend
+  // if (!expenses || expenses.length === 0) {
+  //   throw json(
+  //     { message: "Could not find any expenses." },
+  //     {
+  //       status: 404,
+  //       statusText: "No expenses found!",
+  //     }
+  //   );
+  // } else {
+  //   return expenses;
+  // }
+
+  return expenses;
 }
+
+// instead of getting the root catch boundary we can define our own.
+// but in our case we can't show Add Expense button. Application breaks
+// export function CatchBoundary({ error }) {
+//   return <p>Error</p>;
+// }
